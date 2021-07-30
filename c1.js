9289760daed74583f01d328d738fd1aeb1687d82
Delbert Aud <delbert.aud@gmail.com>
2021-07-30 14:09:07 -0700

    updates

--- c1.js
+++ c1.js
@@ -1,5 +1,7 @@
 content = ""
+displayedCount = 0
 function findCustomers() {
+    displayedCount = 0
     searchForValue = document.getElementById('searchFor').value.toLowerCase();
     content = "<table id=searchTable";
     companies.forEach(checkContains);
@@ -9,20 +11,22 @@ function findCustomers() {
     }
 function checkContains(item, index) {
     if (item.toLowerCase().includes(searchForValue)) {
+        if (displayedCount == 20) { return; }
         fields = item.split(":");
         ccCode = fields[1].trim();
         line = "<tr><td><div onclick=pickCustomer(item1)>item2</div></td></tr>";
         line = line.replace("item1", "'" + ccCode + "'");
         line = line.replace("item2", item);
         content += line;
+        displayedCount += 1;
     }
 }
 function pickCustomer(itemToFind) {
     for (index in companies) {
         if (companies[index].includes(itemToFind)) {
             document.getElementById("searchFor").value = companies[index];
+            findCustomers();
             break;
         }
-    }
-    
+    }  
 }
