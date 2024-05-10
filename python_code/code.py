21a91168bd012c503c48854048d72a739767ee16
Delbert Aud <delbert.aud@gmail.com>
2024-05-10 16:06:41 -0700

    updates

---
+++ python_code/code.py
@@ -0,0 +1,21 @@
+# type: ignore
+import openpyxl as xl
+from faker import Faker
+
+wb = xl.Workbook()
+
+ws = wb.active
+ws.title = "Contacts"
+
+fake = Faker()
+
+for row in range(1, 11):
+    if row == 1:
+        ws.append(["Name", "Phone Number"])
+        continue
+    ws.append([
+        fake.name(),
+        fake.phone_number()
+        ])
+
+wb.save("contacts.xlsx")
